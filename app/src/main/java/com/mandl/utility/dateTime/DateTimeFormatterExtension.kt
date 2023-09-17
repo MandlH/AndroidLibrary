efdb6ecbef944d654e1dd1f81608fc6a54868f1c
haraldmandl <harald.mandl@edu.fh-joanneum.at>
2023-09-17 18:58:50 +0200

    Add DateTimeFormatter

---
+++ app/src/main/java/com/mandl/utility/dateTime/DateTimeFormatterExtension.kt
@@ -0,0 +1,101 @@
+package com.mandl.utility.dateTime
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+// Extension function for LocalDate to format it as a string
+fun LocalDate.format(pattern: String): String {
+    val formatter = DateTimeFormatter.ofPattern(pattern)
+    return this.format(formatter)
+}
+
+// Extension function for LocalDateTime to format it as a string
+fun LocalDateTime.format(pattern: String): String {
+    val formatter = DateTimeFormatter.ofPattern(pattern)
+    return this.format(formatter)
+}
+
+fun Date.getFirstDayOfMonth(): Date {
+    val calendar = Calendar.getInstance()
+    calendar.time = this
+    calendar.set(Calendar.DAY_OF_MONTH, 1)
+    calendar.set(Calendar.HOUR_OF_DAY, 0)
+    calendar.set(Calendar.MINUTE, 0)
+    calendar.set(Calendar.SECOND, 0)
+    calendar.set(Calendar.MILLISECOND, 0)
+    return calendar.time
+}
+
+fun Date.getLastDayOfMonth(): Date {
+    val calendar = Calendar.getInstance()
+    calendar.time = this
+    calendar.add(Calendar.MONTH, 1)
+    calendar.set(Calendar.DAY_OF_MONTH, 1)
+    calendar.add(Calendar.DATE, -1)
+    calendar.set(Calendar.HOUR_OF_DAY, 23)
+    calendar.set(Calendar.MINUTE, 59)
+    calendar.set(Calendar.SECOND, 59)
+    calendar.set(Calendar.MILLISECOND, 999)
+    return calendar.time
+}
+
+// Add or Subtract Days
+fun Date.addDays(days: Int): Date {
+    val calendar = Calendar.getInstance()
+    calendar.time = this
+    calendar.add(Calendar.DAY_OF_MONTH, days)
+    return calendar.time
+}
+
+
+// Add or Subtract Months
+fun Date.addMonths(months: Int): Date {
+    val calendar = Calendar.getInstance()
+    calendar.time = this
+    calendar.add(Calendar.MONTH, months)
+    return calendar.time
+}
+
+// Add or Subtract Years
+fun Date.addYears(years: Int): Date {
+    val calendar = Calendar.getInstance()
+    calendar.time = this
+    calendar.add(Calendar.YEAR, years)
+    return calendar.time
+}
+
+// Add or Subtract Hours
+fun Date.addHours(hours: Int): Date {
+    val calendar = Calendar.getInstance()
+    calendar.time = this
+    calendar.add(Calendar.HOUR_OF_DAY, hours)
+    return calendar.time
+}
+
+// Add or Subtract Minutes
+fun Date.addMinutes(minutes: Int): Date {
+    val calendar = Calendar.getInstance()
+    calendar.time = this
+    calendar.add(Calendar.MINUTE, minutes)
+    return calendar.time
+}
+
+// Add or Subtract Seconds
+fun Date.addSeconds(seconds: Int): Date {
+    val calendar = Calendar.getInstance()
+    calendar.time = this
+    calendar.add(Calendar.SECOND, seconds)
+    return calendar.time
+}
+
+// Calculate Date Difference in Days
+infix fun Date.daysUntil(endDate: Date): Long {
+    val startCalendar = Calendar.getInstance()
+    startCalendar.time = this
+    val endCalendar = Calendar.getInstance()
+    endCalendar.time = endDate
+    val diffInMillis = endCalendar.timeInMillis - startCalendar.timeInMillis
+    return diffInMillis / (24 * 60 * 60 * 1000)
+}
---
+++ app/src/test/java/com/mandl/utility/dateTime/DateTimeFormatterExtensionTest.kt
@@ -0,0 +1,119 @@
+package com.mandl.utility.dateTime
+
+import org.junit.Assert
+import org.junit.Test
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+class DateTimeFormatterExtensionTest {
+    @Test
+    fun localDateFormatTest(){
+        val date = LocalDate.of(2023, 9, 15)
+
+        val formattedDate = date.format("yyyy-MM-dd")
+        Assert.assertEquals("2023-09-15", formattedDate)
+
+        val formattedDate2 = date.format("dd/MM/yyyy")
+        Assert.assertEquals("15/09/2023", formattedDate2)
+    }
+
+    @Test
+    fun testLocalDateTimeFormat() {
+        val dateTime = LocalDateTime.of(2023, 9, 15, 14, 30, 0)
+
+        val formattedDateTime = dateTime.format("yyyy-MM-dd HH:mm:ss")
+        Assert.assertEquals("2023-09-15 14:30:00", formattedDateTime)
+
+        val formattedDateTime2 = dateTime.format("dd/MM/yyyy HH:mm")
+        Assert.assertEquals("15/09/2023 14:30", formattedDateTime2)
+    }
+
+    @Test
+    fun testGetFirstDayOfMonth() {
+        // Define a test date
+        val date = SimpleDateFormat("yyyy-MM-dd").parse("2023-09-15")
+
+        // Get the first day of the month
+        val firstDayOfMonth = date?.getFirstDayOfMonth()
+
+        // Expected result for September 2023 is "2023-09-01"
+        val expected = SimpleDateFormat("yyyy-MM-dd").parse("2023-09-01")
+
+        Assert.assertEquals(expected, firstDayOfMonth)
+    }
+
+    @Test
+    fun testGetLastDayOfMonth() {
+        // Define a test date
+        val date = SimpleDateFormat("yyyy-MM-dd").parse("2023-09-15")
+
+        // Get the last day of the month
+        val lastDayOfMonth = date?.getLastDayOfMonth()
+
+        // Expected result for September 2023 is "2023-09-30"
+        val expected = SimpleDateFormat("yyyy-MM-dd").parse("2023-09-30")
+
+        // Format the dates as strings with only year, month, and day
+        val expectedFormatted = expected?.let { SimpleDateFormat("yyyy-MM-dd").format(it) }
+        val actualFormatted = lastDayOfMonth?.let { SimpleDateFormat("yyyy-MM-dd").format(it) }
+
+        Assert.assertEquals(expectedFormatted, actualFormatted)
+    }
+
+    @Test
+    fun testDateOperations() {
+        // Define a test date
+        val initialDate = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2023-09-15 12:00:00")
+
+        // Add/Subtract Days
+        val datePlus5Days = initialDate?.addDays(5)
+        val dateMinus2Days = initialDate?.addDays(-2)
+
+        // Add/Subtract Months
+        val datePlus3Months = initialDate?.addMonths(3)
+        val dateMinus1Month = initialDate?.addMonths(-1)
+
+        // Add/Subtract Years (Corrected to add 2 years)
+        val datePlus2Years = initialDate?.addYears(2)
+
+        // Add/Subtract Hours
+        val datePlus6Hours = initialDate?.addHours(6)
+        val dateMinus4Hours = initialDate?.addHours(-4)
+
+        // Add/Subtract Minutes
+        val datePlus30Minutes = initialDate?.addMinutes(30)
+        val dateMinus15Minutes = initialDate?.addMinutes(-15)
+
+        // Add/Subtract Seconds
+        val datePlus45Seconds = initialDate?.addSeconds(45)
+        initialDate?.addSeconds(-10)
+
+        // Calculate Date Difference in Days
+        val endDate = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse("2023-09-20 12:00:00")
+        val daysDifference = initialDate?.daysUntil(endDate)
+
+        Assert.assertEquals("2023-09-20 12:00:00",
+            datePlus5Days?.let { SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(it) })
+        Assert.assertEquals("2023-09-13 12:00:00",
+            dateMinus2Days?.let { SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(it) })
+        Assert.assertEquals("2023-12-15 12:00:00",
+            datePlus3Months?.let { SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(it) })
+        Assert.assertEquals("2023-08-15 12:00:00",
+            dateMinus1Month?.let { SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(it) })
+        Assert.assertEquals("2025-09-15 12:00:00",
+            datePlus2Years?.let { SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(it) })
+        Assert.assertEquals("2023-09-15 18:00:00",
+            datePlus6Hours?.let { SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(it) })
+        Assert.assertEquals("2023-09-15 08:00:00",
+            dateMinus4Hours?.let { SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(it) })
+        Assert.assertEquals("2023-09-15 12:30:00",
+            datePlus30Minutes?.let { SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(it) })
+        Assert.assertEquals("2023-09-15 11:45:00",
+            dateMinus15Minutes?.let { SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(it) })
+        Assert.assertEquals("2023-09-15 12:00:45",
+            datePlus45Seconds?.let { SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(it) })
+        Assert.assertEquals(5L, daysDifference) // Use Long instead of Int
+    }
+
+}
