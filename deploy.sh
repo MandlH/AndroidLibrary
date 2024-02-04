8480819d2bd3d56b518420a7e0589e29f6701c2e
haraldmandl <harald.mandl@edu.fh-joanneum.at>
2024-02-04 14:43:50 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -13,29 +13,13 @@ BLUE_TAG="blue"
 GREEN_TAG="green"
 
 if [ "$BRANCH" == "main" ] || [ "$BRANCH" == "env_testing" ]; then
-  echo "Deploying to Blue environment..."
+  echo "Building and pushing image for Blue environment..."
   docker build -t $IMAGE_NAME:$BLUE_TAG .
-  docker stop $IMAGE_NAME-green || true
-  docker run -d -p 8080:80 --name $IMAGE_NAME-blue $IMAGE_NAME:$BLUE_TAG
-
-  # Download the APK artifacts from CI
-  docker cp $(docker create $IMAGE_NAME:$BLUE_TAG):/github/workspace/app-artifact /tmp/artifacts
-  cp /tmp/artifacts/app-debug.apk /usr/share/nginx/html/recommended.apk
-
-  docker rm $IMAGE_NAME-green || true
   docker push $IMAGE_NAME:$BLUE_TAG
 
 elif [ "$BRANCH" == "env_prod" ]; then
-  echo "Deploying to Green environment (Production)..."
+  echo "Building and pushing image for Green environment (Production)..."
   docker build -t $IMAGE_NAME:$GREEN_TAG .
-  docker stop $IMAGE_NAME-blue || true
-  docker run -d -p 8080:80 --name $IMAGE_NAME-green $IMAGE_NAME:$GREEN_TAG
-
-  # Download the APK artifacts from CI
-  docker cp $(docker create $IMAGE_NAME:$GREEN_TAG):/github/workspace/app-artifact /tmp/artifacts
-  cp /tmp/artifacts/app-debug.apk /usr/share/nginx/html/latest.apk
-
-  docker rm $IMAGE_NAME-blue || true
   docker push $IMAGE_NAME:$GREEN_TAG
 
 else
